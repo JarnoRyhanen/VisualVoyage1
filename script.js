f619968c02770448db8456b3a5316884cb9162ac
John Doe <johndoe@email.com>
2024-03-03 16:26:05 +0200

    Created hamburger navigation for smaller screens

--- script.js
+++ script.js
@@ -25,3 +25,13 @@
  span.onclick = function () {
      modal.style.display = "none";
  }
+
+ function myFunction() {
+
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
