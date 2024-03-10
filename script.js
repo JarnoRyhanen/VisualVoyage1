f362a9ead525a721bbfbdf6a8b74153f01ff347a
John Doe <johndoe@email.com>
2024-03-10 16:53:05 +0200

    Renamed the navigation element
    Added the Header and Section elements

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
- // Get the modal
- var modal = document.getElementById("myModal");
+// Get the modal
+let modal = document.getElementById("myModal");
 
- // Get the image and insert it inside the modal - use its "alt" text as a caption
+function showImage(image) {
+  let modalImg = document.getElementById("modalImage");
+  let captionText = document.getElementById("caption");
 
+  /**
+  Pidetään tää tässä ja mietitään myöhemmin
+  
+  let imageName = image.src.replace(/^.*[\\\/]/, '').split(".");
+  let filepath = "images/imagesHD/" + imageName[0]+"HD." + imageName[1];
+  
+  console.log(filepath);
+  */
 
- function showImage( image) {
-     console.log(image.id);
+  modal.style.display = "block";
+  modalImg.src = image.src;
+  captionText.innerHTML = image.alt;
 
-      var modalImg = document.getElementById("modalImage");
-      var captionText = document.getElementById("caption");
+}
 
-      modal.style.display = "block";
-      modalImg.src = image.src;
-      captionText.innerHTML = image.alt;
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
- }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
 
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
-
- // When the user clicks on <span> (x), close the modal
- span.onclick = function () {
-     modal.style.display = "none";
- }
-
- function openDropDown() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+function openDropDown() {
+  var x = document.getElementById("links");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "collapse";
+  } else {
+    x.style.visibility = "visible";
   }
+}
