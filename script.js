45cf99d234733adaf8b0c715ace6cc3c464cba0e
John Doe <johndoe@email.com>
2024-03-11 15:59:29 +0200

    Minor changes

--- script.js
+++ script.js
@@ -4,16 +4,7 @@ let modal = document.getElementById("myModal");
 function showImage(image) {
   let modalImg = document.getElementById("modalImage");
   let captionText = document.getElementById("caption");
-
-  /**
-  Pidetään tää tässä ja mietitään myöhemmin
   
-  let imageName = image.src.replace(/^.*[\\\/]/, '').split(".");
-  let filepath = "images/imagesHD/" + imageName[0]+"HD." + imageName[1];
-  
-  console.log(filepath);
-  */
-
   modal.style.display = "block";
   modalImg.src = image.src;
   captionText.innerHTML = image.alt;
@@ -23,13 +14,14 @@ function showImage(image) {
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 }
 
+// When the user clicks the hamburger menu icon, shows the mobile navigation
 function openDropDown() {
   let links = document.getElementById("links");
   let section = document.getElementById("section");
